fb909792edb73ab91f6f7006fd5d924a215c7ca9
anilkunchalaece <anilkunchalaece@gmail.com>
2016-08-27 19:27:03 +0530

    TestData Updated

--- testData.py
+++ testData.py
@@ -1,9 +1,27 @@
+
 class TestData(object):
     def __init__(self):
-        self.queDict = {'1':'Who is Your Faviourate Fictional Character ?','2':'What is the Capital Of India ?','3':'What is your Favourate Cricketer ?'}
-        self.optADict = {'1':'Super Man','2':'New Delhi','3':'Ganguly'}
-        self.optBDict = {'1':'Bat Man','2':'Hyderabad','3':'Sachin'}
-        self.optCDict = {'1':'Iron Man','2':'Bangalore','3':'Dravid'}
-        self.optDDict = {'1':'Captain America','2':'Nagpur','3':'Sehwag'}
-
-
+        f = open('anatomy_questions.csv','rt')
+        reader = csv.reader(f)
+        self.keys = []
+        self.queDict = {}
+        self.optADict = {}
+        self.optBDict = {}
+        self.optCDict = {}
+        self.optDDict = {}
+        for row in reader:
+            key = row[0]
+            self.keys.append(key)
+            self.queDict[str(key)] = row[1]
+            self.optADict[str(key)] = row[2]
+            self.optBDict[str(key)] = row[3]
+            self.optCDict[str(key)] = row[4]
+            self.optDDict[str(key)] = row[5]
+        f.close()
+        
+if __name__ == "__main__":
+    import csv,re
+    data = TestData()
+    print "no of Questions"
+    print len(data.keys)
+    
